79ed57a474ed50d44b7b128187a9e0a8b92ae9db
AnyMoe <29625989+AnyMoe@users.noreply.github.com>
2019-05-06 00:36:05 +0800

    Follow the license change.

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
 'use strict';
 
+/*
+Copyright 2019 AnyMoe
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 var parents = chrome.contextMenus.create({ "title": "fuck-ulearning", "contexts": ["all"] });
 chrome.contextMenus.create(
     { "title": chrome.i18n.getMessage("showAnswer"), "parentId": parents, "onclick": genericOnClick, id: 'showAnswer', "contexts": ["all"] });
